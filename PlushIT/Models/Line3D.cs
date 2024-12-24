2f923cbfd3f48056c122a6e871096aac933c9e9a
Payton <30909416+Trosclair@users.noreply.github.com>
2024-12-24 08:15:37 -0600

    Added Line, but haven't implemented it yet

---
+++ PlushIT/Models/Line3D.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlushIT.Models
+{
+    public class Line3D
+    {
+        public Edge3D Edge1 { get; set; }
+        public Edge3D Edge2 { get; set; }
+
+        public Line3D(Edge3D edge1, Edge3D edge2)
+        {
+            Edge1 = edge1;
+            Edge2 = edge2;
+        }
+    }
+}
